55e0c3b763b4d7c097def2fdff8f5e03f0489f27
Jana <jjanakragovic@gmail.com>
2018-06-20 19:37:55 +0200

    das

--- implementacija/laravel/app/Http/Controllers/SessionController.php
+++ implementacija/laravel/app/Http/Controllers/SessionController.php
@@ -14,7 +14,7 @@ use Validator;
 use App\User;
 use App\Ban;
 use App\Rank;
-
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 
@@ -50,7 +50,8 @@ class SessionController extends Controller
             return redirect()->back()->with('banovanSi','Pristup sajtu nije moguć, banovani ste');
         }
     	
-    	if (Auth::attempt(['username' => $username, 'password' => $password])) {
+    	//if (Auth::attempt(['username' => $username, 'password' => $password])) {
+        if(Hash::check($password,Auth::user()->password)){
     		//$user = User::where('username', $username)->update(['online' => 1]);
             $user=User::where('username', $username)->first();
             $user->online=1;
@@ -77,7 +78,7 @@ class SessionController extends Controller
         $rankInfo=json_decode($result);
        
         $rank=$rankInfo[0]->tier." ".$rankInfo[0]->rank;
-        
+        //proveri za koji mod igre vrca rank na poziciji 0 , da li je uvek ranked solo duo na 0
         $rankTable= Rank::where('name',$rank)->first();
         $user->rank_id=$rankTable->id;
         $user->update();
