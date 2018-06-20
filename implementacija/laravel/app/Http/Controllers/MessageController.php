d7ce0b880af83784a755551d23ab07630577d153
Stevan Tulovic <stevan.tulovic@gmail.com>
2018-06-20 15:52:29 +0200

    inbox load issues

--- implementacija/laravel/app/Http/Controllers/MessageController.php
+++ implementacija/laravel/app/Http/Controllers/MessageController.php
@@ -65,17 +65,19 @@ class MessageController extends Controller
 		}
 		else {
 			$focus = $conversations->take(1)->value('id');
-            $conversation = DB::table('conversation')->where('id',$focus);
-            $reading = $conversation->first();
-            if ($reading->user1_id == $id) {
-                if ($reading->user1_read != 1) {
-                    $conversation->update(['user1_read' => 1]);
-                    $untracked = 1;
-                }
-            } else {
-                if ($reading->user2_read != 1) {
-                    $conversation->update(['user2_read' => 1]);
-                    $untracked = 1;
+            if ($focus != null) {
+                $conversation = DB::table('conversation')->where('id',$focus);
+                $reading = $conversation->first();
+                if ($reading->user1_id == $id) {
+                    if ($reading->user1_read != 1) {
+                        $conversation->update(['user1_read' => 1]);
+                        $untracked = 1;
+                    }
+                } else {
+                    if ($reading->user2_read != 1) {
+                        $conversation->update(['user2_read' => 1]);
+                        $untracked = 1;
+                    }
                 }
             }
 		}
